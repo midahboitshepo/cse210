eb2684d0fb2645daed5d3ffe170699c40a6ed2ee
midahboitshepo <mboitshepo@byupathway.edu>
2025-09-10 20:03:22 +0200

    Week 2 resumes

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._jobTitle = "System Analyst";
+        job1._company = "Local Governement";
+        job1._startYear = 2009;
+        job1._endYear = 2010;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Software Engineer";
+        job2._company = "DCDMC";
+        job2._startYear = 2011;
+        job2._endYear = 2013;
+
+        Resume myResume = new Resume();
+        myResume._name = "Midah Boitshepo";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
