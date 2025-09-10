55fa8ce30997b3abf51e344b95c08bf6d7c7e205
midahboitshepo <mboitshepo@byupathway.edu>
2025-09-10 17:44:19 +0200

    Exercise 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        int magicNumber = 0;
+        int userGuess = -1;
+        Random random = new Random();
+        magicNumber = random.Next(1, 101); // Generates a number between 1 and 10
+        Console.WriteLine("What is the magic number ?" + magicNumber); // For testing purposes
+
+        while (userGuess != magicNumber)
+        {
+            Console.Write("What is your guess ? ");
+            userGuess = int.Parse(Console.ReadLine());
+
+            if (userGuess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (userGuess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
+
     }
 }
