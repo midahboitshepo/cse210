4be8514b2249a7967eb40ecfe5c96bdfd0a59215
midahboitshepo <mboitshepo@byupathway.edu>
2025-09-17 14:52:05 +0200

    Fractions

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+        Franction f1 = new Franction();
+        Console.WriteLine($"{f1}");
+        Console.WriteLine(f1.GetDecimalValue());
+
+        Franction f2 = new Franction(5);
+        Console.WriteLine($"{f2}");
+        Console.WriteLine(f2.GetDecimalValue());
+
+        Franction f3 = new Franction(3, 4);
+        Console.WriteLine($"{f3}");
+        Console.WriteLine(f3.GetDecimalValue());
+
+        Franction f4 = new Franction(1, 3);
+        Console.WriteLine($"{f4}");
+        Console.WriteLine(f4.GetDecimalValue());
+
     }
 }
