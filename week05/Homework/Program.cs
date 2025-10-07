894960be66040e92fed449588329ac0c40db8eb0
midahboitshepo <mboitshepo@byupathway.edu>
2025-10-07 06:22:21 +0200

    Assignment base

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -2,8 +2,9 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    public static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        Assignment assignment = new Assignment("Midah", "Programming with C#");
+        Console.WriteLine(assignment.GetSummary());
     }
 }
