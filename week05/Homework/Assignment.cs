43ee3796fbcb350687a79f6096fd8091f0c73054
midahboitshepo <mboitshepo@byupathway.edu>
2025-10-07 22:50:44 +0200

    Final

--- week05/Homework/Assignment.cs
+++ week05/Homework/Assignment.cs
@@ -1,7 +1,7 @@
 public class Assignment
 {
-    private string _studentName;
-    private string _topic;
+    protected string _studentName;
+    protected string _topic;
 
     public Assignment(string studentName, string topic)
     {
--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -6,5 +6,11 @@ class Program
     {
         Assignment assignment = new Assignment("Midah", "Programming with C#");
         Console.WriteLine(assignment.GetSummary());
+
+        MathAssignment mathAssignment = new MathAssignment("Bothepha", "Multiplication", "Section 10.3", "Problems 20-29");
+        Console.WriteLine(mathAssignment.GetSummary());
+
+        WritingAssignment writingAssignment = new WritingAssignment("Nancy", "European History", "The Causes of World War II");
+        Console.WriteLine(writingAssignment.GetWritingInformation());
     }
 }
